4371a1e315eca51f6d530f0da59182c79768a668
Klaudia Błażyczek <k.blazyczek@tkhtechnology.com>
2021-09-23 10:28:27 +0200

    Implement Cart button component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import { Fragment } from "react";
 
 import mealsImage from '../../assets/meals.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = props => {
     return (
         <Fragment>
             <header className={styles.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button> {/* just for now - we don't have card component yet*/}
+                <HeaderCartButton />
             </header>
             <div className={styles['main-image']}>
                 <img src={mealsImage} alt='A table full of food' />
