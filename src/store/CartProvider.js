e079f470840587122c375a4ada12617f9b330e1d
klaudia <blazyczek.klaudia@gmail.com>
2021-11-15 17:54:57 +0100

    Making Items Removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,25 +10,50 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        const updatedTotalAmount =
+            state.totalAmount + action.item.price * action.item.amount;
 
-        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+        const existingCartItem = state.items[existingCartItemIndex];
         let updatedItems;
 
-        if (existingCartItemIndex === -1) {
-            updatedItems = state.items.concat(action.item);
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
         } else {
-            const clonedArray = [...state.items];
-            clonedArray[existingCartItemIndex].amount += action.item.amount;
-            updatedItems = clonedArray;
+            updatedItems = state.items.concat(action.item);
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
+    }
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
         }
 
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
         };
-    } else if (action.type === 'REMOVE') {
-        return
     }
 
     return defaultCartState;
